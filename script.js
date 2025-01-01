1b50fd476109e51371a2e8738663c95726488b21
mahdikillerajaj <mahdikh7889@gmail.com>
2025-01-01 23:48:05 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function showSlide(slideId) {
-    const slides = document.querySelectorAll('.slide');
-    slides.forEach(slide => {
-        slide.style.display = 'none';
-    });
-    document.getElementById(slideId).style.display = 'block';
-}
